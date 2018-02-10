d69ea7734f7bdabd186464a67ecc85c14f6cd99d
ganesh444 <ganeshteppani444@gmail.com>
2018-02-10 15:09:22 +0530

    Initial commit

---
+++ src/main/java/com/hello/gan/GanApplication.java
@@ -0,0 +1,12 @@
+package com.hello.gan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GanApplication.class, args);
+	}
+}
